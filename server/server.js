7102b819ea861d359194b5b8f9d835a0bd166e9f
NKudratov <khudratov88@gmail.com>
2018-08-11 12:50:35 -0700

    Added GET /todos/id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var { mongoose } = require('./db/mongoose.js');
 var { Todo } = require('./models/todo.js');
 var { User } = require('./models/user.js');
+var { ObjectID } = require('mongodb');
 
 var app = express();
 
@@ -19,6 +20,29 @@ app.post('/todos', (req, res) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send(JSON.stringify(todo, undefined, 2));
+    }, (e) => {
+        res.status(40).send();
     });
 });
 
